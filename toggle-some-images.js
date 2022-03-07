8b24f71a2319c2f2d93c1d70315d51fb0da78638
noname774300 <noname774300@gmail.com>
2022-03-07 22:24:20 +0900

    enable to toggle images

--- toggle-some-images.js
+++ toggle-some-images.js
@@ -1,6 +1,13 @@
 // javascript:
 (() => {
+  const className = "__bookmarklets-toggle-some-images";
+  const styles = document.querySelectorAll(`.${className}`);
+  if (styles.length > 0) {
+    styles.forEach((style) => style.remove());
+    return;
+  }
   const style = document.createElement("style");
+  style.classList.add(className);
   document.body.appendChild(style);
   style.sheet.insertRule("img, source { visibility: hidden !important; }");
   style.sheet.insertRule("* { background: none !important; }");
